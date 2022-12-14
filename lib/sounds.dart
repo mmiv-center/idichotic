4217b227342f3b69512c6a32813aef0d471d1451
Daniel Mjøs Røli <Daniel.Roli@student.uib.no>
2022-12-14 17:54:40 +0100

    FIX: Changed the return icon.

--- lib/sounds.dart
+++ lib/sounds.dart
@@ -105,6 +105,12 @@ class ListenAppState extends State<ListenApp> {
             shadowColor: Colors.transparent,
             backgroundColor: Colors.transparent,
             foregroundColor: Colors.black,
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back_ios)
+            ),
     ),
         body: Center(
         //
